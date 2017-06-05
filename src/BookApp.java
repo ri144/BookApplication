7fe5a0b0b4d82c94b07cc42873b6e393ca958181
Rob Gross <144big@gmail.com>
2017-06-05 11:07:26 -0400

    Added Database class

--- src/BookApp.java
+++ src/BookApp.java
@@ -6,6 +6,8 @@ public class BookApp {
 		NumberFormat formatter = new DecimalFormat("#0.00");
 		Book b = new Book("1984","George Orwell","Dystopian society where big brother is watching you", 5, 9.99f);
 		b.getDisplayText();
-		System.out.print(formatter.format(b.requestPricingOfBooks(3)));
+		System.out.println(formatter.format(b.requestPricingOfBooks(3)));
+		BookDatabase mydata = new BookDatabase();
+		System.out.print(mydata.findBookFromSKU("Java1001").getPrice());
 	}
 }
